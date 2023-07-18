d8bb502787926452de9d04316c4d21ecfd79db50
Sathvik <99279970+rssnama@users.noreply.github.com>
2023-07-18 11:25:09 +0530

    Add files via upload

---
+++ HtoEOCR.py
@@ -0,0 +1,63 @@
+import cv2  
+from matplotlib import pyplot as plt
+import pytesseract
+from PIL import Image
+from flask import Flask, request, render_template, redirect,jsonify
+import urllib.request
+import os
+from englisttohindi.englisttohindi import EngtoHindi
+
+#from firebase_admin import credentials, firestore, initialize_app
+
+app = Flask(__name__)
+#cred = credentials.Certificate('key.json')
+#default_app = initialize_app(cred)
+#db = firestore.client()
+#todo_ref = db.collection('todos')
+
+#app.config["IMAGE_UPLOADS"] = "C:/Users/Sathvik/Downloads/OCR/images"
+# app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG","JPG","JPEG"]
+
+from werkzeug.utils import secure_filename
+
+
+#image_file = "images/vc2.png"
+#img = cv2.imread(image_file)
+@app.route('/', methods=["GET", "POST"])
+
+
+def upload_image():
+
+    global img
+    try:
+        reqRef = request.get_json(force=True)
+        img = reqRef["imgUrl"]
+        urllib.request.urlretrieve(img,"gfg.jpeg")
+    # img = Image.open("gfg.jpeg")
+    # img.show()
+
+    # print("abc"+ img)
+    
+        
+        
+        path_to_tesseract=r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
+        #Imagepath='gfg.jpeg'
+        pytesseract.tesseract_cmd=path_to_tesseract
+        text=pytesseract.image_to_string(Image.open("gfg.jpeg"))
+        res = EngtoHindi(text)
+
+        return jsonify({
+            #"ocr_result": text,
+            "hindi_result": res,
+
+        })
+
+    except Exception as e:
+        return jsonify({
+            "weigthURL": "No found",
+            "error": str(e)
+        })
+    
+port = int(os.environ.get('PORT', 8080))
+if __name__ == '__main__':
+    app.run(debug=True, port=2000)
